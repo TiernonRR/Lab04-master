8af9536cb30f0e8bc688815a049d173658aa4296
TiernonRR <tiernon.riesenmy@gmail.com>
2019-03-06 09:08:46 -0600

    continued work on js

--- formChecker.js
+++ formChecker.js
@@ -1,4 +1,34 @@
-functiono checkForm(){
+function checkQuantities(){
+    let firstItem = document.getElementById("itemOne").value;
+    let secondItem = document.getElementById("itemTwo").value;
+    let thirdItem = document.getElementById("itemThree").value;
+
+    if(firstItem == null){
+        return(false;)
+    }
+    if(secondItem == null){
+        return(false;)
+    }
+    if(thirdItem == null){
+        return(false;)
+    }
+
+    return(true);
+}
+
+function radioSelect(){
+    let firstRadio = document.getElementById('free').checked;
+    let secondRadio = document.getElementById('overNight').checked;
+    let thirdRadio = document.getElementById('threeDay').checked;
+
+    if(firstRadio || secondRadio || thirdRadio){
+        return true;
+    }
+
+    return false;
+}
+
+function checkForm(){
     let quanCheck   = checkQuantities();
     let radioSelect = checkRadio();
     
