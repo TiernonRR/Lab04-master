65d61618e4b329739e25176c477a2921e963c02c
TiernonRR <tiernon.riesenmy@gmail.com>
2019-03-06 20:09:56 -0600

    Final Changes

--- multTable.php
+++ multTable.php
@@ -1,9 +1,17 @@
 <?php
 echo "<table>";
-  for($i =1; $i <= 100; $i+=1){
+  for($i =0; $i <= 100; $i+=1){
     echo "<tr>";
-    for($j=1; $j <= 100; $j+=1){
+    for($j=0; $j <= 100; $j+=1){
+      if($i == 0 ){
+        echo " <td> " . $j . " </td>";
+      }
+      else if($j == 0){
+        echo " <td> " . $i . " </td>";
+      }
+      else{
         echo " <td> " . $i*$j . " </td>";
+      }
     }
     echo "</tr>";
   }
